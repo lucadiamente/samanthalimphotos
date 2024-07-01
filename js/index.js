6963d06a52150c4d165bfcb09fc65fc8d5e395d3
Luca Diamente <lucadiamente@gmail.com>
2024-07-01 15:14:17 +0900

    Move index.html file to root for gitHub Pages

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
-$(document).on("scroll", function() {
-    var pageTop = $(document).scrollTop();
-    var pageBottom = pageTop + $(window).height();
-    var tags = $(".tag");
-  
-    for (var i = 0; i < tags.length; i++) {
-      if (document.getElementById("about").classList.contains("visible") && document.getElementById("portfolio").classList.contains("visible")) {
-        return
-      } else {
-        var tag = tags[i];
-        if ($(tag).position().top < pageBottom) {
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    if (
+      document.getElementById("about").classList.contains("visible") &&
+      document.getElementById("portfolio").classList.contains("visible") &&
+      document.getElementById("contact").classList.contains("visible")
+    ) {
+      return;
+    } else {
+      var tag = tags[i];
+      if ($(tag).position().top < pageBottom) {
         $(tag).addClass("visible");
-        } else {
+      } else {
         $(tag).removeClass("visible");
-        }
       }
     }
-  });
-  
-  
+  }
+});
